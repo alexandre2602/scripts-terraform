0e7cdfbc73123e7bc82721f5886ceb212832a8a4
Cesar Sturion <csturion@gmail.com>
2020-04-07 17:34:51 -0300

    criacao do sqs

---
+++ sqs/sqs_queue.tf
@@ -0,0 +1,13 @@
+resource "aws_sqs_queue" "sqs_queue" {
+  name                      = "gudiao-labs-sqs-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+  fifo_queue = true
+
+  tags = {
+    Name = "gudiao-labs-sqs-queue"
+    Environment = "dev"
+  }
+}
